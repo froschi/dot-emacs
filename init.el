1868c66c19f05984f41f2068a169674b0bc539ce
Thorsten Fischer <thorsten@froschi.org>
2013-04-06 21:28:59 +0200

    Load packages. Load yasnippet.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
-;; Hurra, emacs!
+;;; Hurra, emacs!
 
-; First of all, aesthetics.
+;; First of all, aesthetics.
 ; I do not want that annoying splash screen.
 (setq inhibit-splash-screen t)
 ; My default font for the console.
@@ -11,10 +11,17 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 
-; Package sources
+;; Packages
 (require 'package)
+; Packages sources
 (add-to-list 'package-archives
   '("marmelade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
+; Initialize
 (package-initialize)
+; Package: yanisppet
+; Source: https://github.com/capitaomorte/yasnippet
+; Introduction video: http://www.youtube.com/watch?v=ZCGmZK4V7Sg
+(require 'yasnippet)
+(yas-global-mode 1)
